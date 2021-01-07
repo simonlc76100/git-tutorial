8dda673aee2d3ba71c48fa5009000f7a08f41531
Simon Lellouche <simon.lellouche@edu.devinci.fr>
2021-01-07 13:23:56 -0800

    Add os.system(clear) to clear the terminal at certain points during the game, if the user fail to find the number and since the terminal is cleared after that, add a reminder of what the user's last guess was in the endgame message

--- guessing_game.py
+++ guessing_game.py
@@ -2,13 +2,16 @@
 
 # The random package is needed to choose a random number
 import random
- 
+import os
+
+os.system("clear")
+
 # Define the game in a function
 def guess_loop():
     # This is the number the user will have to guess, chosen randomly in between 1 and 100
     number_to_guess = random.randint(1, 100)
     
-    print("Welcome to the guessing game, please enter a number that matches the difficulty:\n1: Novice\n2: Normal\n3: Expert\n4: Hardcore\n5: only 1 shot!\n")
+    print("Welcome to the guessing game, please enter a number that matches the difficulty:\n\n1: Novice\n2: Normal\n3: Expert\n4: Hardcore\n5: only 1 shot !\n")
     
     # loop with try, except, else to manage invalid input
     while True:
@@ -32,24 +35,26 @@ def guess_loop():
             if 1 <= difficulty <= 5:
                 break
             else:
-                print("Please, choose a valid number !")
+                print("Invalid input, please enter an integer in between 1 and 5 !")
                 continue
             
         except ValueError as err:
             print("Invalid input, please enter an integer")
         else:
             break
+    
+    os.system("clear")
         
     if difficulty==5:
-        print("\nI have in mind a number in between 1 and 100, can you find it?\nYou only have 1 guess available")
+        print("\nI have in mind a number in between 1 and 100, can you find it?\nYou only have 1 guess available\n")
     else:
-        print("\nI have in mind a number in between 1 and 100, can you find it?\nYou have", limit ,"guesses available")
+        print("\nI have in mind a number in between 1 and 100, can you find it?\nYou have", limit ,"guesses available\n")
  
     # Replay the question until the user finds the correct number or reaches the limit
     while True:
         try:
             # Read the number the user inputs
-            guess = int(input())
+            guess = int(input("number: "))
             
             # condition to check if user enter a number between 1 and 100
             if 1 <= guess <= 100:
@@ -58,15 +63,18 @@ def guess_loop():
                     count=0
                     # if the user find the correct number, exit the game
                     if guess==number_to_guess:
+                        os.system("clear")
                         print("\nYou just found the number, it was indeed", guess ,"and it took you 1 guess to succeed!")
                         return
                     else:
                         # if the user reaches the limit, warn him/her of his/her failure and exit the game
-                        print("\nNoob ! You failed to find the number ! It was", number_to_guess ,"!")
+                        os.system("clear")
+                        print("\nNoob ! You failed to find the number ! Your last guess was", guess ,"and the correct number was", number_to_guess ,"!")
                         return
                 else:
                     if count==limit and guess != number_to_guess:
-                        print("\nNoob ! You failed to find the number ! It was", number_to_guess ,"!")
+                        os.system("clear")
+                        print("\nNoob ! You failed to find the number ! Your last guess was", guess ,"and the correct number was", number_to_guess ,"!")
                         return
                     if guess > number_to_guess:
                         print("The number to guess is lower")
@@ -75,10 +83,12 @@ def guess_loop():
                     else:
                         # condition in case the user find the number in only one guess when he/she has multiple guesses
                         if count==1:
+                            os.system("clear")
                             print("\nYou just found the number, it was indeed", guess ,"and it took you 1 guess to succeed!")
                             return
                         # condition in case the user uses more than one guess to find the correct number
                         else:
+                            os.system("clear")
                             print("\nYou just found the number, it was indeed", guess ,"and it took you", count ,"guesses to succeed!")
                             return
                         
